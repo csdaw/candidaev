7ac50fac13f4497f4409b7ae9af39f3a57e7dcb5
Charlotte Dawson <csdawson@ltu.edu.au>
2019-02-04 14:52:55 +1100

    Add `plot_venn` function
     - Add function to plot an up to 5-way venn diagram using VennDiagram
       package.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -3,3 +3,57 @@ plot_dendro <- function(lfq_mat) {
   hc <- hclust(dis)
   plot(hc)
 }
+
+plot_venn <- function(mats, plot = TRUE, file = NULL, output = c("tiff", "png"), ...) {
+  # Supress futile logger message from VennDiagram package
+  futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
+
+  # use matrix rownames for comparison
+  comparison <- mapply(rownames, mats)
+
+  # plot venn or get venn partition lists
+  if(plot == FALSE) {
+    venn <- get.venn.partitions(comparison)
+    return(venn)
+  } else if(plot == TRUE & is.null(file) == TRUE) {
+    venn_plot <- venn.diagram(x = comparison,
+                              filename = NULL,
+                              height = 800,
+                              width = 800,
+                              resolution = 300,
+                              lty = "solid",
+                              lwd = 2)
+    grid.draw(venn_plot)
+  } else if(plot == TRUE & is.null(file) == FALSE) {
+    venn_plot <- venn.diagram(x = comparison,
+                              filename = NULL,
+                              imagetype = "tiff",
+                              compression = "lzw",
+                              height = 800,
+                              width = 800,
+                              resolution = 300,
+                              lty = "solid",
+                              lwd = 2)
+    if(output == "tiff") {
+      tiff(filename = file,
+           width = 800,
+           height = 800,
+           res = 300,
+           units = "px",
+           pointsize = 12,
+           type = "cairo",
+           compression = "lzw")
+      grid.draw(venn_plot)
+      dev.off()
+    } else if (output == "png") {
+      png(filename = file,
+          width = 800,
+          height = 800,
+          res = 300,
+          units = "px",
+          type = "cairo")
+      grid.draw(venn_plot)
+      dev.off()
+    }
+  }
+}
