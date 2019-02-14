73660470b4b8ab350e0835ba1659aeea877b1135
Charlotte Dawson <csdawson@ltu.edu.au>
2019-02-14 11:56:31 +1100

    Fix uniprot.rda and uniprot.R
    Forgot match_id was refactored to switch argument position. Therefore,
    uniprot.rda was incorrect.
    
    Also refactor uniprot.R for brevity.

--- data-raw/reference/uniprot.R
+++ data-raw/reference/uniprot.R
@@ -21,75 +21,32 @@ uniprot <- read.delim(file = "data-raw/reference/20180616_uniprot-proteome_UP000
                          stringsAsFactors = FALSE)
 
 # add column names
-colnames(uniprot) <- c("UP_accession",
-                       "CGDID",
-                       "UP_gene_name",
-                       "Gene_name_ol",
-                       "Gene_name_orf",
-                       "Protein_name",
-                       "Function",
-                       "Subcellular_location",
-                       "Mass_(Da)",
-                       "Length_(aa)",
-                       "Sequence",
-                       "Transmembrane",
-                       "Topology",
-                       "Signal_peptide",
-                       "GO_all",
-                       "GO_CC",
-                       "GO_MF",
-                       "GO_BP",
-                       "GO_IDs",
-                       "UP_status")
+colnames(uniprot) <- c("UP_accession", "CGDID", "UP_gene_name", "Gene_name_ol", "Gene_name_orf",
+                       "Protein_name", "Function", "Subcellular_location", "Mass_(Da)",
+                       "Length_(aa)", "Sequence", "Transmembrane", "Topology", "Signal_peptide",
+                       "GO_all", "GO_CC", "GO_MF", "GO_BP", "GO_IDs", "UP_status")
 
 # define list of bad identifiers to replace
-bad_ids <- c("CAALFM_CR",
-             "CAALFM_C1",
-             "CAALFM_C2",
-             "CAALFM_C3",
-             "CAALFM_C4",
-             "CAALFM_C5",
-             "CAALFM_C6",
-             "CAALFM_C7",
-             "CA$",
-             "WA$",
-             "CB$",
-             "WB$")
+bad_ids <- c("CAALFM_CR", "CAALFM_C1", "CAALFM_C2", "CAALFM_C3", "CAALFM_C4", "CAALFM_C5",
+             "CAALFM_C6", "CAALFM_C7", "CA$", "WA$", "CB$", "WB$")
 
 # define list of good identifiers to replace bad_ids
-good_ids <- c("CR_",
-              "C1_",
-              "C2_",
-              "C3_",
-              "C4_",
-              "C5_",
-              "C6_",
-              "C7_",
-              "C_A",
-              "W_A",
-              "C_B",
-              "W_B")
+good_ids <- c("CR_", "C1_", "C2_", "C3_", "C4_", "C5_", "C6_", "C7_", "C_A", "W_A", "C_B", "W_B")
 
 # replace bad identifiers with good identifiers
-uniprot$Gene_name_orf <- stringi::stri_replace_all_regex(uniprot$Gene_name_orf,
-                                                            bad_ids,
-                                                            good_ids,
-                                                            FALSE)
+uniprot$Gene_name_orf <- stringi::stri_replace_all_regex(uniprot$Gene_name_orf, bad_ids,
+                                                            good_ids, vectorize_all = FALSE)
 
-uniprot$Gene_name_ol <- stringi::stri_replace_all_regex(uniprot$Gene_name_ol,
-                                                           bad_ids,
-                                                           good_ids,
-                                                           FALSE)
+uniprot$Gene_name_ol <- stringi::stri_replace_all_regex(uniprot$Gene_name_ol, bad_ids,
+                                                           good_ids, vectorize_all = FALSE)
 
 # if orf column is blank, insert identifier from ol column
 uniprot <- fill_blank(uniprot, "Gene_name_orf", "Gene_name_ol")
 
 # create a new CGDID column using info from cgd.rda and Gene_name_ol
-uniprot$CGDID2 <- as.character(cgd$CGDID[match(uniprot$Gene_name_ol,
-                                                        cgd$Systematic_name)])
+uniprot$CGDID2 <- as.character(cgd$CGDID[match(uniprot$Gene_name_ol, cgd$Systematic_name)])
 
-uniprot$CGDID3 <- as.character(cgd$CGDID[match(uniprot$Gene_name_orf,
-                                                        cgd$Systematic_name)])
+uniprot$CGDID3 <- as.character(cgd$CGDID[match(uniprot$Gene_name_orf, cgd$Systematic_name)])
 
 # fill in blanks in unip_table CGDID column
 uniprot <- fill_blank(uniprot, "CGDID", "CGDID2")
@@ -100,18 +57,14 @@ uniprot <- fill_na(uniprot, "CGDID", "CGDID3")
 uniprot$CGDID <- gsub(";$", "", uniprot$CGDID)
 
 # add some CGD columns (using UniProt CGDIDs for crossreference)
-uniprot$CGD_gene_name <- match_id(uniprot$CGDID, cgd, "Gene_name", "CGDID")
-uniprot$CGD_feature_name <- match_id(uniprot$CGDID, cgd, "Feature_name", "CGDID")
-uniprot$CGD_systematic_name <- match_id(uniprot$CGDID, cgd, "Systematic_name", "CGDID")
-uniprot$CGD_description <- match_id(uniprot$CGDID, cgd, "Description", "CGDID")
-
-# remove CGDID2, CGDID3 ol, orf column
-uniprot <- within(uniprot, rm(CGDID2, CGDID3, Gene_name_ol, Gene_name_orf))
-
-# rearrange columns
 uniprot <- uniprot %>%
+  mutate(CGD_gene_name = match_id(CGDID, cgd, "CGDID", "Gene_name"),
+         CGD_feature_name = match_id(CGDID, cgd, "CGDID", "Feature_name"),
+         CGD_systematic_name = match_id(CGDID, cgd, "CGDID", "Systematic_name"),
+         CGD_description = match_id(CGDID, cgd, "CGDID", "Description")) %>%
   select(UP_accession, CGDID, UP_gene_name, CGD_gene_name, CGD_feature_name,
-         CGD_systematic_name, Protein_name, CGD_description, everything())
+         CGD_systematic_name, Protein_name, CGD_description, everything(),
+         -CGDID2, CGDID3, Gene_name_ol, Gene_name_orf)
 
 # write output .rda file
-usethis::use_data(uniprot, overwrite = TRUE)
+usethis::use_data(uniprot)
