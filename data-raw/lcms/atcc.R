1ca6026c54177729317a192e1994f84493a863f6
Charlotte Dawson <csdawson@ltu.edu.au>
2019-02-28 15:25:10 +1100

    Change atcc column name format
     - Sample names in atcc.Rda are in an inappropriate format as they
       begin with a number, for example "10231_EV1".
     - Change column names in /data-raw/lcms/atcc.R to be, for example
       "A1_EV1" or "A9_EV1".
     - Change atcc_exp.txt so labels are for example "A10231_EV1" etc.

--- data-raw/lcms/atcc.R
+++ data-raw/lcms/atcc.R
@@ -8,5 +8,11 @@ atcc <- read.table("data-raw/lcms/atcc-proteinGroups.txt", sep = "\t", header = 
 # remove duplicate periods in column names
 atcc <- remove_dotdot(atcc)
 
+# rename columns so sample names don't start with a number
+colnames(atcc) <- stringi::stri_replace_all_regex(str = colnames(atcc),
+                                                  pattern = c("10231_EV", "10231_W", "90028_EV", "90028_W"),
+                                                  replacement = c("A1_EV", "A1_W", "A9_EV", "A9_W"),
+                                                  vectorize_all = FALSE)
+
 # write output .rda file
 usethis::use_data(atcc, overwrite = TRUE)
