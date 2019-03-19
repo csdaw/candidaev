56693c8a0321ebb52d3ed5f1ad0abd6c6878deb3
Charlotte Dawson <csdawson@ltu.edu.au>
2019-03-19 11:16:05 +1100

    Refine figure 4

--- R/plot_mds.R
+++ R/plot_mds.R
@@ -11,14 +11,18 @@
 #'
 #' @export
 #'
-plot_mds <- function(mat) {
+plot_mds <- function(mat, mat_labels, shape_size = 2) {
   mds <- limma::plotMDS(mat, plot = FALSE)
 
   coord <- data.frame(dim1 = mds[["x"]],
                       dim2 = mds[["y"]]) %>%
     tibble::rownames_to_column(var = "sample") %>%
-    mutate(sample = stringr::str_extract(.[["sample"]], "[^_]+"))
+    mutate(sample = stringr::str_extract(.[["sample"]], "[^_]+"),
+           label = mat_labels)
 
-  ggplot(coord, aes(x = dim1, y = dim2, colour = sample)) +
-    geom_point()
+  ggplot(coord, aes(x = dim1, y = dim2)) +
+    geom_point(aes(colour = sample, shape = sample), size = shape_size) +
+    geom_text(aes(label = mat_labels), hjust = 1.4, vjust = 0.5) +
+    xlab(label = "Dimension 1") +
+    ylab(label = "Dimension 2")
 }
