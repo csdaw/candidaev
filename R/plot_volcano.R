63f3568c97025243fa37d8ae36aba3046173c6fb
Charlotte Dawson <csdawson@ltu.edu.au>
2019-04-15 16:04:14 +1000

    Finalise `plot_volcano`
     - `plot_volcano` now allows slightly more customisation
    
     - Update `plot_volcano` calls in manuscript.Rmd
    
     - Delete redundant `plot_volcano2` modified function from the DEP
       package
    
     - Remove documentation for deleted functions `plot_volcano2` and
       `plot_heatmap2`
    
     - Closes #1, #7

--- R/plot_volcano.R
+++ R/plot_volcano.R
@@ -4,36 +4,64 @@
 #'
 #' @param data Description.
 #' @param p_val Description.
-#' @param lfc Description.
-#' @param grp Description.
-#' @param colour Description.
-#' @param label Description.
+#' @param log2fc Description.
+#' @param group Description.
+#' @param label_fun Description.
+#' @param point_size Description.
+#' @param label_size Description.
+#' @param x_lim Description.
+#' @param y_lim Description.
+#' @param legend_params Description.
+#' @param axis_params Description.
 #'
 #' @return Description.
 #'
 #' @examples
-#' #example
+#'
+#' # example
 #'
 #' @export
-plot_volcano <- function(data, p_val, lfc, grp, label) {
+plot_volcano <- function(data, p_val, log2fc, group, label_fun,
+                         point_size = 1, label_size = 2,
+                         x_lim = c(-10, 10), y_lim = c(0, 10),
+                         legend_params = c("left", 11, 10),
+                         axis_params = c("black", 10)) {
+
+  # enquo data columns and label function
   p_val <- rlang::enquo(p_val)
-  lfc <- rlang::enquo(lfc)
-  grp <- rlang::enquo(grp)
-  label <- rlang::enquo(label)
+  log2fc <- rlang::enquo(log2fc)
+  group <- rlang::enquo(group)
+  label_fun <- rlang::enquo(label_fun)
 
+  # plot all data that has a valid log2fc value
+  p <- ggplot(data = dplyr::filter(data, !is.na(!!log2fc)),
+              mapping = aes(x = !!log2fc, y = -log10(!!p_val))) +
+    geom_point(mapping = aes(colour = !!group), size = point_size) +
+    theme_classic(base_size = 11)
 
-  ggplot(data = dplyr::filter(data, !is.na(!!lfc)),
-         mapping = aes(x = !!lfc, y = -log10(!!p_val))) +
-    geom_point(mapping = aes(colour = !!grp), size = 0.6) +
-    theme_classic(base_size = 8) +
-    theme(legend.position = "left",
-          legend.title = element_text(size = 11, face = "bold"),
-          axis.text = element_text(colour = "black", size = 10)) +
+  # adjust legend position, text sizes, text colours
+  p <- p +
+    theme(legend.position = legend_params[1],
+          legend.title = element_text(size = legend_params[2], face = "bold"),
+          legend.text = element_text(size = legend_params[3]),
+          axis.text = element_text(colour = axis_params[1], size = axis_params[2]))
+
+  # adjust size of dots in legend and add text annotations to certain
+  # points on the plot. Also add axis labels
+  p <- p +
     guides(colour = guide_legend(override.aes = list(size = 3))) +
-    ggrepel::geom_text_repel(data = dplyr::filter(data, !is.na(!!lfc)),
-                             mapping = aes(label = !!label),
+    ggrepel::geom_text_repel(data = dplyr::filter(data, !is.na(!!log2fc)),
+                             mapping = aes(label = !!label_fun),
                              show.legend = FALSE,
-                             size = 2) +
+                             size = label_size)
+
+  # add axis limits and axis labels
+  p <- p +
+    scale_x_continuous(limits = x_lim, expand = c(0,0)) +
     xlab(expression(log[2]("FC"))) +
+    scale_y_continuous(limits = y_lim, expand = c(0,0)) +
     ylab(expression(-log[10]("Adj. p-value")))
+
+  # return plot
+  p
 }
